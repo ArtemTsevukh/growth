b681cae009f49ba47e08723b92d2e6212ada38a6
Artom Tsevukh <artom.tsevukh@robirent.com>
2023-10-11 22:08:20 +0300

    supabase2

--- app/app/post/page.tsx
+++ app/app/post/page.tsx
@@ -1,10 +1,12 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { createClient } from "@supabase/supabase-js"
 import { cookies } from "next/headers"
 
 async function fetchData() {
-    const supabase = createServerComponentClient({
-        cookies
-    })
+    const supabase = createClient(
+       'https://lfebbmgccwvkghwnkhfj.supabase.co', 
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZWJibWdjY3d2a2dod25raGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MzY3MzMsImV4cCI6MjAxMjExMjczM30.5gsuWrHFfYj3F0akQCHwfnFmp82EqJfowWBdc2lenzw'
+    )
     const {data, error} = await supabase.from('posts').select('*')
 
     if (error) {
