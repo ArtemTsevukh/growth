9e6e50e4020f10ef41262b283fe3fb9a67e50be4
Artom Tsevukh <artom.tsevukh@robirent.com>
2023-10-13 19:29:19 +0300

    env variable

--- app/app/post/page.tsx
+++ app/app/post/page.tsx
@@ -4,8 +4,8 @@ import { cookies } from "next/headers"
 
 async function fetchData() {
     const supabase = createClient(
-       'https://lfebbmgccwvkghwnkhfj.supabase.co', 
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZWJibWdjY3d2a2dod25raGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MzY3MzMsImV4cCI6MjAxMjExMjczM30.5gsuWrHFfYj3F0akQCHwfnFmp82EqJfowWBdc2lenzw'
+      process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
     )
     const {data, error} = await supabase.from('posts').select('*')
 
@@ -21,14 +21,14 @@ export default async function Post() {
     
     return (
         <div>
-      <h1>
-        {blogPosts.map((post) => <div key={post.id}>
-          <p>{post.title}</p>
-          <h2>{post.content}</h2>
-          <div>{post.created_at}</div>
-         </div> 
-         )}
-      </h1>
+          <h1>
+          {blogPosts.map((post) => <div key={post.id}>
+            <p>{post.title}</p>
+            <h2>{post.content}</h2>
+            <div>{post.created_at}</div>
+          </div> 
+          )}
+        </h1>
     </div>
     )
 }
